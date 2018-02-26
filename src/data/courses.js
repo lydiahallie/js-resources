893f9273f59c6d2d85dcc4434fb184834f81d294
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-02-26 13:20:11 +0100

    Add fake data

---
+++ src/data/courses.js
@@ -0,0 +1,59 @@
+export const COURSES = [
+  {
+    name: 'JavaScript For Beginners',
+    category: 'online',
+    rating: 4,
+    price: 40,
+    decription: 'The best course for JavaScript beginners',
+    img: ''
+  },
+  {
+    name: 'JavaScript For Intermediate Programmers',
+    category: 'book',
+    rating: 4,
+    price: 10,
+    decription: 'The best course for JavaScript beginners',
+  },
+  {
+    name: 'Advanced JavaScript',
+    category: 'online',
+    rating: 2,
+    price: 99,
+    decription: 'The best course for JavaScript beginners',
+  },
+  {
+    name: 'JavaScript For Beginners',
+    category: 'book',
+    rating: 5,
+    price: 0,
+    decription: 'The best course for JavaScript beginners',
+  },
+  {
+    name: 'JavaScript For Beginners Docs',
+    category: 'docs',
+    rating: 1,
+    price: 45,
+    decription: 'The best course for JavaScript beginners',
+  },
+  {
+    name: 'JavaScript For Beginners',
+    category: 'online',
+    rating: 2,
+    price: 50,
+    decription: 'The best course for JavaScript beginners',
+  },
+  {
+    name: 'JavaScript For Beginners',
+    category: 'book',
+    rating: 4,
+    price: 120,
+    decription: 'The best course for JavaScript beginners',
+  },
+  {
+    name: 'JavaScript For Beginners',
+    category: 'online',
+    rating: 4,
+    price: 50,
+    decription: 'The best course for JavaScript beginners',
+  }
+]
