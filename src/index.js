1b747a0551d136dc3fb32d7cbe27c566940fd4f5
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-03-08 18:36:02 +0100

    Change router path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import FavoriteResources from './components/AddResource';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+<BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/favorites" component={FavoriteResources} />
+    </Switch>
+  </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
