217dc4a909a59f07a734fab183e59ce03ee9326f
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-04-25 12:11:17 +0200

    Add sass loader to webpack config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -151,6 +151,12 @@ module.exports = {
               cacheDirectory: true,
             },
           },
+          // Add sass.
+          {
+            test: /\.sass$/,
+            include: paths.appSrc,
+            loaders: ["style", "css", "sass"]
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
@@ -198,7 +204,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.sass$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
